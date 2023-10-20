9636782f8c7b9325e4345ef188654b9ed550dd61
keithdavey <keith@lovepopcards.com>
2023-10-19 20:07:30 -0500

    bug fixes

--- src/scripts/login-echo.sh
+++ src/scripts/login-echo.sh
@@ -3,4 +3,4 @@
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 TO=$(circleci env subst "${PARAM_TO}")
 # If for any reason the TO variable is not set, default to "World"
-echo $GITHUB_TOKEN | docker login https://docker.pkg.github.com -u lovepopcards --password-stdin
+echo "$GITHUB_TOKEN" | docker login https://docker.pkg.github.com -u lovepopcards --password-stdin
