534ac01cdbfcb735a902c2215b767d2e6ea6d16d
keithdavey <keith@lovepopcards.com>
2023-10-19 20:57:10 -0500

    bug fixes

--- src/scripts/docker_push_login.sh
+++ src/scripts/docker_push_login.sh
@@ -3,4 +3,4 @@
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 # If for any reason the TO variable is not set, default to "World"
 AWS_ECR_PWD=`aws ecr get-login-password --region us-west-2`
-docker login --username AWS --password ${AWS_ECR_PWD} ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com
+docker login --username AWS --password "${AWS_ECR_PWD}" "${AWS_ACCOUNT_ID}".dkr.ecr.us-west-2.amazonaws.com
--- src/scripts/terraform_apply.sh
+++ src/scripts/terraform_apply.sh
@@ -2,8 +2,6 @@
 # This example uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 # If for any reason the TO variable is not set, default to "World"
-GIT_SHA=$(git rev-parse --short HEAD)
-terraform apply \
- <<parameters.additional-terraform-arguments>> \
- -var "container_version=${CIRCLE_BRANCH}.${GIT_SHA}" -var "vpc_id=${AWS_VPC_ID}" \
+GIT_SHA=$(git rev-parse --short HEAD) terraform apply \
+ <<parameters.additional-terraform-arguments>> -var "container_version=${CIRCLE_BRANCH}.${GIT_SHA}" -var "vpc_id=${AWS_VPC_ID}" \
 -var "env=${AWS_ENV}" -var "account=${AWS_ACCOUNT_ID}" -var "aws_region=us-west-2" -auto-approve terraform/
